1fa72f5e45c1330ff68531ab50f7dd1d99b5aa7b
Thakur Abhay 2 <146329899+abhaypratapsingh7704866570@users.noreply.github.com>
2026-01-05 23:37:04 +0530

    configrating for domain

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
+  server: {
+    host: true, // This makes it listen on all local IPs
+    port: 5173,
+    allowedHosts: [
+      'webdeveloper.engineer',
+      '.webdeveloper.engineer' // The dot allows subdomains too
+    ]
+  }
 })
